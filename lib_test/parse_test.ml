b11d8ad40d980b775a212a814c7730069975d81d
David Scott <dave.scott@eu.citrix.com>
2013-11-11 12:56:43 +0000

    test: switch to the "nobody" user so file read/only vs read/write checks work
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib_test/parse_test.ml
+++ lib_test/parse_test.ml
@@ -71,7 +71,7 @@ let check_parent_parent_dir () =
   Vhd_IO.create_dynamic ~filename ~size:0L () >>= fun vhd ->
   let leaf_path = Filename.(concat (concat tmp_file_dir "leaves") "leaf.vhd") in
   let leaf_dir = Filename.dirname leaf_path in
-  (try Unix.mkdir leaf_dir 0o0644 with _ -> ());
+  (try Unix.mkdir leaf_dir 0o0755 with _ -> ());
   Vhd_IO.create_difference ~filename:leaf_path ~parent:vhd ~relative_path:true () >>= fun vhd' ->
   (* Make sure we can open the leaf *)
   Vhd_IO.openfile leaf_path false >>= fun vhd'' ->
@@ -84,8 +84,8 @@ let check_readonly () =
   let filename = make_new_filename () in
   Vhd_IO.create_dynamic ~filename ~size:0L () >>= fun vhd ->
   Vhd_IO.close vhd >>= fun () ->
-  Unix.chmod filename 0o400;
-  Vhd_IO.openfile filename true >>= fun vhd ->
+  Unix.chmod filename 0o444;
+  Vhd_IO.openfile filename false >>= fun vhd ->
   Vhd_IO.close vhd
 
 let fill_sector_with pattern =
@@ -204,6 +204,10 @@ let _ =
   let check_empty_snapshot size =
     Printf.sprintf "check_empty_snapshot_%Ld" size
     >:: (fun () -> Lwt_main.run (check_empty_snapshot size)) in
+
+  (* Switch to the 'nobody' user so we can test file permissions *)
+  let nobody = Unix.getpwnam "nobody" in
+  Unix.setuid nobody.Unix.pw_uid;
 
   let suite = "vhd" >:::
     [
